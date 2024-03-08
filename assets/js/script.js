95e58db0f7a77862bd039117a00f702d40dc3353
Roberto Velasquez <rvelasquez579@gmail.com>
2024-03-07 21:41:52 -0300

    Se agrega seccion Contacto y Footer, junto con sus estilos y logicas de JavaScript

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+$("#send").click(function(){
+    alert('Gracias por contactarnos')
+})
